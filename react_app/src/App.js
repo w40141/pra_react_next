cc2a0fabbe28962c96071b26101780d3a9f66061
w40141 <w.40141@gmail.com>
2021-11-23 18:39:06 +0900

    finish sec 4-3

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,71 +1,58 @@
-// import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import React, { Component } from "react";
-// import Rect from "./Rect";
 
-let theme = {
-  light: {
-    styles: {
-      backgroundColor: "#f0f9ff",
-      color: "#00f",
-    },
-    head: "bg-primary text-white display-4 mb-4",
-    alert: "alert alert-primary my-3",
-    text: "text-primary m-3",
-    foot: "py-4",
-  },
-  dark: {
-    styles: {
-      backgroundColor: "#336",
-      color: "#eef",
-    },
-    head: "bg-secondary text-white display-4 mb-4",
-    alert: "alert alert-dark my-3",
-    text: "text-light m-3",
-    foot: "py-4",
-  },
-};
+function AlertMessage(props) {
+  return (
+    <div className="alert alert-primary h5 text-primary">
+      <h5>{props.msg}</h5>
+    </div>
+  );
+}
 
-const ThemeContext = React.createContext(theme.dark);
+function App() {
+  const [val, setVal] = useState(1000);
+  const [tax1, setTax1] = useState(0);
+  const [tax2, setTax2] = useState(0);
+  const [msg, setMsg] = useState(<p>set a number...</p>);
 
-class App extends Component {
-  static contextType = ThemeContext;
-  render() {
-    return (
+  const doChange = (e) => {
+    setVal(e.target.value);
+  };
+
+  useEffect(() => {
+    let res = (
       <div>
-        <h1 className="bg-primary text-white display-4">React</h1>
-        <div className="container">
-          <Title value="Content page"/>
-          <Message value="This is content sample."/>
-          <Message value="これはテーマのサンプルです。"/>
-          <div className={this.context.foot}></div>
+        <p>軽減税率( 8%): {tax1} 円</p>
+        <p>軽減税率(10%): {tax2} 円</p>
+      </div>
+    );
+    setMsg(res);
+  }, [tax1, tax2]);
+
+  useEffect(() => {
+    setTax1(Math.floor(val * 1.08));
+  }, [val]);
+
+  useEffect(() => {
+    setTax2(Math.floor(val * 1.1));
+  }, [val]);
+
+  return (
+    <div>
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <div className="container">
+        <h4 className="my-3">Hooks sample</h4>
+        <AlertMessage msg={msg} />
+        <div className="form-group">
+          <label>Input: </label>
+          <input type="number" className="form-control" onChange={doChange} />
         </div>
+        <button className="btn btn-primary" onClick={doChange}>
+          Calc
+        </button>
       </div>
-    );
-  }
-}
-
-class Title extends Component {
-  static contextType = ThemeContext;
-
-  render() {
-    return (
-      <div className={this.context.alert}>
-        <h2 style={this.context.style}>{this.props.value}</h2>
-      </div>
-    );
-  }
-}
-
-class Message extends Component {
-  static contextType = ThemeContext;
-  render() {
-    return (
-      <div className={this.context.alert}>
-        <p className={this.context.text}>{this.props.value}</p>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
