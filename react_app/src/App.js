c429840e5f2b671a9f73969d7f7f4012f8abd7da
w40141 <w.40141@gmail.com>
2021-11-23 16:52:44 +0900

    finish sec 4

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,69 +1,71 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
-import Rect from "./Rect";
+// import Rect from "./Rect";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+let theme = {
+  light: {
+    styles: {
+      backgroundColor: "#f0f9ff",
+      color: "#00f",
+    },
+    head: "bg-primary text-white display-4 mb-4",
+    alert: "alert alert-primary my-3",
+    text: "text-primary m-3",
+    foot: "py-4",
+  },
+  dark: {
+    styles: {
+      backgroundColor: "#336",
+      color: "#eef",
+    },
+    head: "bg-secondary text-white display-4 mb-4",
+    alert: "alert alert-dark my-3",
+    text: "text-light m-3",
+    foot: "py-4",
+  },
+};
+
+const ThemeContext = React.createContext(theme.dark);
 
 class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      msg: "count start!",
-      count: 0,
-      flag: true,
-    };
-    this.doAction = this.doAction.bind(this);
-  }
-
-  doAction(event) {
-    this.setState({
-      count: this.state.count + 1,
-      msg: "*** count:" + this.state.count + " ***",
-      flag: !this.state.flag,
-    });
-  }
+  static contextType = ThemeContext;
   render() {
     return (
       <div>
         <h1 className="bg-primary text-white display-4">React</h1>
         <div className="container">
-          <p className="subtitle">Show message.</p>
-                    {this.state.flag? 
-          <div className="alert alert-warning text-right">
-            <p className="h5 mb-4">count: {this.state.msg}</p>
-            </div>:
-          <div className="alert alert-warning text-left">
-            <p className="h5 mb-4">{this.state.msg}</p>
-            </div>
-                    }
-          <div className="alert alert-warning text-center">
-            <button className="btn btn-primary" onClick={this.doAction}>
-              Click
-            </button>
-          </div>
+          <Title value="Content page"/>
+          <Message value="This is content sample."/>
+          <Message value="これはテーマのサンプルです。"/>
+          <div className={this.context.foot}></div>
         </div>
       </div>
     );
   }
 }
+
+class Title extends Component {
+  static contextType = ThemeContext;
+
+  render() {
+    return (
+      <div className={this.context.alert}>
+        <h2 style={this.context.style}>{this.props.value}</h2>
+      </div>
+    );
+  }
+}
+
+class Message extends Component {
+  static contextType = ThemeContext;
+  render() {
+    return (
+      <div className={this.context.alert}>
+        <p className={this.context.text}>{this.props.value}</p>
+      </div>
+    );
+  }
+}
+
 export default App;
