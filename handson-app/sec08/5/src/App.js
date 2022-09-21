b9d29469caf8763d42a3a695d995e9959055806c
w40141 <w.40141@gmail.com>
2022-09-21 23:45:34 +0900

    choro: 不要なものを消した

--- handson-app/sec08/5/src/App.js
+++ handson-app/sec08/5/src/App.js
@@ -1,42 +1,12 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {GitHubUser} from "./GitHubUser";
 import {RepositoryReadme} from "./RepositoryReadme";
 import {SearchForm} from "./SearchForm";
 import {UserRepositories} from "./UserRepositories";
-// import {GraphQLClient} from "graph-request";
-
-// const query = `
-//     query findRepos($login:String!) {
-//         user(login:$login) {
-//             login
-//             name
-//             location
-//             avatar_url: avatarUrl
-//             repositories(first:100) {
-//                 totalCount
-//                 nodes {
-//                     name
-//                 }
-//             }
-//         }
-//     }
-// `;
-//
-// const client = new GraphQLClient("https://api.github.com/graphql", {
-//     headers: {
-//         Authorization: `Bearer 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`
-//     }
-// });
 
 export default function App() {
     const [login, setLogin] = useState();
     const [repo, setRepo] = useState();
-    // const [userDate, setUserDate] = useState();
-    //
-    // useEffect(() => {
-    //     client.request(query, {login}).then(({user}) => user).then(setUserDate)
-    //         .catch(console.error)
-    // }, [client, query, login])
 
     const handleSearch = login => {
         if (login) return setLogin(login);
