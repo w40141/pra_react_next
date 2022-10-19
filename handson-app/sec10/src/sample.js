f23c62f3ca4b2efb9cdc10ef6d2b678de9bf91ab
w40141 <w.40141@gmail.com>
2022-10-19 18:51:33 +0900

    finish sec10

---
+++ handson-app/sec10/src/sample.js
@@ -0,0 +1,23 @@
+const info = ({
+    file = __filename,
+    dir = __dirname
+}) => (
+    <p>
+        {dir}: {file}
+    </p>
+);
+
+function name(params) {
+
+}
+
+function gnar() {
+    const [nickname, setNickname] = useState("dude")
+    return <h1>gnarly</h1>
+}
+
+switch (gnar) {
+    default:
+        console.log("gnarly");
+        break;
+}
